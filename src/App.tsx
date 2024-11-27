2b3260d8a5c1762df8fc464053901efb395daa13
jenniferom <jenniferom0009@gmail.com>
2024-11-27 10:44:47 -0500

    refactor: restructure files and remove unused directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import './App.css'
-import Advice from './components/Advice'
+import { useEffect, useState } from 'react';
+import { fetchAdvice } from './services/api';
 
-function App() {
+
+const App: React.FC = () => {
+  const [advice, setAdvice] = useState<string>('');
+  const [id, setSlipId] = useState<number | null>(null);
+
+  useEffect(() => {
+      const loadAdvice = async () => {
+          try {
+              const data = await fetchAdvice();
+              setAdvice(data.advice);
+              setSlipId(data.id);
+          } catch (error) {
+              console.error("Error al cargar el consejo:", error);
+          }
+      };
+      loadAdvice();
+  }, []);
+
 
   return (
     <>
-      <Advice />
+      <div className='container'>
+            <h3>Advice #{id}</h3>
+            <p>"{advice}"</p>
+        </div>
     </>
   )
 }
